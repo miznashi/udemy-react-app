1ec0880c39a554bdd2f5482d57cc86620c3df580
miztraghetto <aria.hahi.avvenire@gmail.com>
2020-05-07 00:37:28 +0900

    more-jsx sample program

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function App() {
   return (
-    <div><h1>Hello World!</h1></div>
+    <React.Fragment>
+      <h1>Hello World!</h1>
+      <label htmlFor="bar">bar</label>
+      <input
+        type="text"
+        onChange={() => {console.log("test change text")}}
+      />
+    </React.Fragment>
   );
 }
 
