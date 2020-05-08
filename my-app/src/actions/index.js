dcd0d4220a54bec11a454b60d1f2ce99a3d546ee
miztraghetto <aria.hahi.avvenire@gmail.com>
2020-05-08 21:37:26 +0900

    implement action and action creator

---
+++ my-app/src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMEN'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: 'INCREMENT'
+})
+
+export const decrement = () => ({
+  type: 'DECREMENT'
+})
