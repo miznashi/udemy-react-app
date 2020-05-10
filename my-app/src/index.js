acdfd71bb3f69e68922978a842fb3fd487a32162
miztraghetto <aria.hahi.avvenire@gmail.com>
2020-05-11 02:46:25 +0900

    implement delete feature

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,22 +6,28 @@ import thunk from 'redux-thunk'; //追加
 
 // 32. イベント新規作成画面への画面遷移を実装する
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import './index.css';
 import reducer from './reducers'; //追加
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
+import EventsShow from './components/events_show';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const enhancer = process.env.NODE_ENV === 'development' ?
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+
+  const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        
-        <Route exact path="/event/new" component={EventsNew} />
+        <Route path="/event/new" component={EventsNew} />
+        <Route path="/events/:id" component={EventsShow} />
         <Route exact path="/" component={EventsIndex} />
+        <Route exact path="/events" component={EventsIndex} />
       </Switch>
     </BrowserRouter>
   </Provider>,
--- my-app/src/reducers/events.js
+++ my-app/src/reducers/events.js
@@ -1,23 +1,14 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import { READ_EVENTS, DELETE_EVENT } from '../actions'
 
 export default (events = {}, action) => {
   switch (action.type) {
-
-    // ☓ 扱いにくい
-    // [
-    //   {"id": 1, "title1": "Let's have an event 1!", "body": "This is the body for event 1!" },
-    //   { "id": 2, "title2": "Let's have an event 2!", "body": "This is the body for event 2!" }
-    // ]
-    // ◯ 扱いやすく
-    // [
-    //   1: {"id": 1, "title1": "Let's have an event 1!", "body": "This is the body for event 1!" },
-    //   2: { "id": 2, "title2": "Let's have an event 2!", "body": "This is the body for event 2!" }
-    // ]
-    // 上記の用にするパッケージが「lodash」 これをインポートする
-
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
+    case DELETE_EVENT:
+      // console.log(action.id)
+      delete events[action.id]
+      return { ...events }
     default:
       return events
   }
